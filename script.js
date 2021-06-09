9fd18aefdd2bbcc25015bc8be71774de2e780af5
“isab554b” <isab554b@stud.kea.dk>
2021-06-09 10:57:42 +0200

    burger js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+  const burgerBtn = document.querySelector("#burger_btn");
+  burgerBtn.addEventListener("click", openMenu);
+}
+
+function openMenu() {
+  const burgerBtn = this;
+  const menu = document.querySelector("#menu");
+
+  this.removeEventListener("click", openMenu);
+  this.classList.add("open");
+  menu.classList.add("open");
+  this.addEventListener("click", closeMenu);
+}
+
+function closeMenu() {
+  const burgerBtn = this;
+  const menu = document.querySelector("#menu");
+
+  this.removeEventListener("click", closeMenu);
+  menu.classList.remove("open");
+  this.addEventListener("click", openMenu);
+}
